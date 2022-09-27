25feaa54e0d31c1dbfc1b7144cc620cd5c0df405
Paul Kevin <paultitude@gmail.com>
2022-09-27 13:52:50 +0300

    Site service complete and controller

---
+++ src/main/java/com/doltics/commerce/controller/store/SiteController.java
@@ -0,0 +1,45 @@
+package com.doltics.commerce.controller.store;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.doltics.commerce.response.GenericMessage;
+import com.doltics.commerce.service.SiteService;
+import com.doltics.commerce.utils.StatusUtils;
+
+@RestController("site")
+public class SiteController {
+	
+	@Autowired
+	private SiteService siteService;
+	
+	/**
+	 * 
+	 * <p>
+	 * Validate site connection.
+	 * </p>
+	 *
+	 * @param domain
+	 * @param apiKey
+	 * @return
+	 */
+	@GetMapping( value = "/validate")
+	public GenericMessage validateSite(
+			@RequestHeader(name = "domain", required = true) String domain,
+			@RequestHeader(name = "api_key", required = true) String apiKey) {
+		CompletableFuture<GenericMessage> response = siteService.validateSite(domain, apiKey);
+		CompletableFuture.allOf(response).join();
+		try {
+			return response.get();
+		} catch (InterruptedException e) {
+			return new GenericMessage(StatusUtils.ERROR, "An error occured " + e.getMessage());
+		} catch (ExecutionException e) {
+			return new GenericMessage(StatusUtils.ERROR, "An error occured " + e.getMessage());
+		}
+	}
+}
--- src/main/java/com/doltics/commerce/service/SiteService.java
+++ src/main/java/com/doltics/commerce/service/SiteService.java
@@ -1,8 +1,67 @@
 package com.doltics.commerce.service;
 
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import com.doltics.commerce.entity.stores.Site;
+import com.doltics.commerce.repository.stores.SiteRepository;
+import com.doltics.commerce.response.GenericMessage;
+import com.doltics.commerce.utils.StatusUtils;
 
 @Service
 public class SiteService {
 
+	@Autowired
+	private SiteRepository siteRepository;
+	
+	/**
+	 * 
+	 * <p>
+	 * Validate site
+	 * </p>
+	 *
+	 * @param domain
+	 * @param apiKey
+	 * @return
+	 */
+	@Async("asyncExecutor")
+	public CompletableFuture<GenericMessage> validateSite(String domain, String apiKey) {
+		boolean valid = isValidSite(domain, apiKey);
+		if (valid) {
+			return CompletableFuture.completedFuture(new GenericMessage(StatusUtils.SUCCESS, "Site connected")); 
+		}
+		return CompletableFuture.completedFuture(new GenericMessage(StatusUtils.ERROR, "Invalid site")); 
+	}
+	
+	/**
+	 * 
+	 * <p>
+	 * Get site
+	 * </p>
+	 *
+	 * @param domain
+	 * @param apiKey
+	 * @return
+	 */
+	public Site getSite(String domain, String apiKey) {
+		return siteRepository.findByDomainAndApiKey(domain, apiKey);
+	}
+	
+	/**
+	 * 
+	 * <p>
+	 * Validate site
+	 * </p>
+	 *
+	 * @param domain
+	 * @param apiKey
+	 * @return
+	 */
+	public boolean isValidSite(String domain, String apiKey) {
+		Site site = getSite(domain, apiKey);
+		return site != null;
+	}
 }
